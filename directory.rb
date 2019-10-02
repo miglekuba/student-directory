762b018983fe57df16f61ed6731387419ea96ece
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-02 16:37:51 +0100

    rewrited methods

--- directory.rb
+++ directory.rb
@@ -1,25 +1,36 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp 
+  while !name.empty? do    
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  filtered_students = students.select do |student|
+    student[:name][0] == "B" && student[:name].length < 12
+  end
+  filtered_students.each_with_index do |student, index|
+    puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_while(students)
+  counter = 0 
+  while counter < students.size
+   keys = students.values_at(counter)
+   puts "#{counter} "+" #{keys[0].values[0]}"
+    counter+=1
   end
 end
 
@@ -27,6 +38,8 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+
+students = input_students
 print_header
-print(students)
+print_while(students)
 print_footer(students)
