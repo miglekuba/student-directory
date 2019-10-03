258da9be52e79b8c3ad8dcd4da06bf7fff3394b9
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-03 15:13:30 +0100

    additional information

--- directory.rb
+++ directory.rb
@@ -6,31 +6,49 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+
   students = []
-  name = gets.chomp 
-  while !name.empty? do    
-    students << {name: name, cohort: :november}
+
+  puts "Enter the first name:"
+  first_name = gets.chomp 
+
+  while !first_name.empty?  
+  puts "Enter your last name:"
+  last_name = gets.chomp 
+  if last_name.empty?
+    puts "Please enter your last name!"
+    last_name = gets.chomp 
+  end
+
+  puts "Enter you date of birth:"
+  date_birth = gets.chomp
+  if date_birth.empty?
+    puts "Please enter you date of birth!"
+    date_birth = gets.chomp
+  end
+  puts "Enter your favorite movie:"
+  movie = gets.chomp  
+  if movie.empty?
+  puts "eyyyyy enter the name of the movie!!!"
+  end
+  puts "Enter your cohort"
+  cohort = gets.chomp
+  if cohort.empty?
+    puts "enter the cohort please!"
+  end
+
+    students << {first_name: first_name, last_name: last_name, date_birth: date_birth, movie: movie, cohort: cohort}
+
     puts "Now we have #{students.count} students"
-    name = gets.chomp
+    puts "Enter next student's name:"
+    first_name = gets. chomp 
   end
   students
 end
 
 def print(students)
-  filtered_students = students.select do |student|
-    student[:name][0] == "B" && student[:name].length < 12
-  end
-  filtered_students.each_with_index do |student, index|
-    puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_while(students)
-  counter = 0 
-  while counter < students.size
-   keys = students.values_at(counter)
-   puts "#{counter} "+" #{keys[0].values[0]}"
-    counter+=1
+  students.each do |student|
+    puts "#{student[:first_name]} #{student[:last_name]} #{student[:date_birth]} #{student[:movie]} (#{student[:cohort]}"
   end
 end
 
@@ -41,5 +59,5 @@ end
 
 students = input_students
 print_header
-print_while(students)
+print(students)
 print_footer(students)
