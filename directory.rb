03f2057c0b654691158340f80ae9a07efdafdb95
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-03 16:04:14 +0100

    tweeks

--- directory.rb
+++ directory.rb
@@ -8,10 +8,15 @@ def input_students
   puts "To finish, just hit return twice"
 
   students = []
+  months = [
+    :November,
+    :December,
+    :January,
+    :April,
+    :May ]
 
   puts "Enter the first name:"
   first_name = gets.chomp 
-
   while !first_name.empty?  
   puts "Enter your last name:"
   last_name = gets.chomp 
@@ -32,11 +37,17 @@ def input_students
   puts "eyyyyy enter the name of the movie!!!"
   end
   puts "Enter your cohort"
-  cohort = gets.chomp
+  cohort = gets.chomp.capitalize.to_sym
   if cohort.empty?
-    puts "enter the cohort please!"
+    cohort = :April 
   end
-
+  while !months.include?(cohort)
+    puts "Error. Hit enter to use default cohort"
+  cohort = gets. chomp.capitalize.to_sym
+  if cohort.empty?
+  cohort = :November
+  end
+end
     students << {first_name: first_name, last_name: last_name, date_birth: date_birth, movie: movie, cohort: cohort}
 
     puts "Now we have #{students.count} students"
@@ -48,7 +59,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:first_name]} #{student[:last_name]} #{student[:date_birth]} #{student[:movie]} (#{student[:cohort]}"
+    puts "#{student[:first_name]} #{student[:last_name]} #{student[:date_birth]} #{student[:movie]} (#{student[:cohort]})"
   end
 end
 
